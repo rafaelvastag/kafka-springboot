500d77aa51f920914039e5c70d696dabcaf23d87
rafaelvastag <rgvastag@gmail.com>
2023-01-18 21:43:12 -0300

    add consumer

---
+++ str-consumer/str-consumer/src/test/java/br/com/vastag/strconsumer/StrConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vastag.strconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
